b306319dfaf8317c31014e4194f10d90d3692e7b
Helal <imhelal.dev@gmail.com>
2025-01-02 18:22:16 +0600

    Icon issue fix

--- includes/classes.php
+++ includes/classes.php
@@ -1,3 +1,4 @@
 <?php
+require_once WPSM_PLUGIN_DIR . 'includes/core/custom-nav-walker.php';
 require_once WPSM_PLUGIN_DIR . 'includes/WPSM_Template.php';
 require_once WPSM_PLUGIN_DIR . 'includes/WPSM_Menu.php';
